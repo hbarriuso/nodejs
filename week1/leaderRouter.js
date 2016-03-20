d476beedf5e0652c9a924dfd855b30ccab9da419
Héctor Hugo Barriuso Mata <hbarriuso@gmail.com>
2016-03-20 23:26:21 +0100

    Changed dishId to leaderId and promotionId

--- week1/leaderRouter.js
+++ week1/leaderRouter.js
@@ -23,24 +23,24 @@ router.route('/')
         res.end('Deleting all leaders');
 });
 
-router.route('/:dishId')
+router.route('/:leaderId')
 .all(function(req,res,next) {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       next();
 })
 
 .get(function(req,res,next){
-        res.end('Will send details of the leader: ' + req.params.dishId +' to you!');
+        res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
 })
 
 .put(function(req, res, next){
-        res.write('Updating the leader: ' + req.params.dishId + '\n');
+        res.write('Updating the leader: ' + req.params.leaderId + '\n');
     res.end('Will update the leader: ' + req.body.name +
             ' with details: ' + req.body.description);
 })
 
 .delete(function(req, res, next){
-        res.end('Deleting leader: ' + req.params.dishId);
+        res.end('Deleting leader: ' + req.params.leaderId);
 });
 
 module.exports = router;
--- week1/promoRouter.js
+++ week1/promoRouter.js
@@ -23,24 +23,24 @@ router.route('/')
         res.end('Deleting all promotions');
 });
 
-router.route('/:dishId')
+router.route('/:promotionId')
 .all(function(req,res,next) {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       next();
 })
 
 .get(function(req,res,next){
-        res.end('Will send details of the promotion: ' + req.params.dishId +' to you!');
+        res.end('Will send details of the promotion: ' + req.params.promotionId +' to you!');
 })
 
 .put(function(req, res, next){
-        res.write('Updating the promotion: ' + req.params.dishId + '\n');
+        res.write('Updating the promotion: ' + req.params.promotionId + '\n');
     res.end('Will update the promotion: ' + req.body.name +
             ' with details: ' + req.body.description);
 })
 
 .delete(function(req, res, next){
-        res.end('Deleting promotion: ' + req.params.dishId);
+        res.end('Deleting promotion: ' + req.params.promotionId);
 });
 
 module.exports = router;
