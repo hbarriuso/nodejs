1db32a29bce466459f5eaf88a2732f9236d0f80d
Héctor Hugo Barriuso Mata <hbarriuso@gmail.com>
2016-03-20 20:12:42 +0100

    week 1 assignment

---
+++ week1/promoRouter.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+
+router.use(bodyParser.json());
+
+router.route('/')
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+})
+
+.get(function(req,res,next){
+        res.end('Will send all the promotions to you!');
+})
+
+.post(function(req, res, next){
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+
+.delete(function(req, res, next){
+        res.end('Deleting all promotions');
+});
+
+router.route('/:dishId')
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+})
+
+.get(function(req,res,next){
+        res.end('Will send details of the promotion: ' + req.params.dishId +' to you!');
+})
+
+.put(function(req, res, next){
+        res.write('Updating the promotion: ' + req.params.dishId + '\n');
+    res.end('Will update the promotion: ' + req.body.name +
+            ' with details: ' + req.body.description);
+})
+
+.delete(function(req, res, next){
+        res.end('Deleting promotion: ' + req.params.dishId);
+});
+
+module.exports = router;
