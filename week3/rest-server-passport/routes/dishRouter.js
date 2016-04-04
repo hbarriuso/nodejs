b1e39c6817cf597335c21de94c6f279119f2e5f3
Héctor Hugo Barriuso Mata <hbarriuso@gmail.com>
2016-04-04 22:42:32 +0200

    Used verifyAdmin for Task 2 of assignment for Week 3

--- week3/rest-server-passport/routes/dishRouter.js
+++ week3/rest-server-passport/routes/dishRouter.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 
 var Dishes = require('../models/dishes');
 
-var Verify = require('./verify')
+var Verify = require('./verify');
 
 var dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
